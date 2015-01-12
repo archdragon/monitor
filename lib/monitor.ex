441e008084d496f95df349f8d7f4133042e47dc6
Przemek <przemek@krowinski.eu>
2015-01-12 22:50:45 +0100

    Add Repo worker

--- lib/monitor.ex
+++ lib/monitor.ex
@@ -9,6 +9,7 @@ defmodule Monitor do
     children = [
       # Define workers and child supervisors to be supervised
       # worker(Monitor.Worker, [arg1, arg2, arg3])
+      worker(Repo, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
